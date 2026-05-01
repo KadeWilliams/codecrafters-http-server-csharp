9588cf650016b08db559b83fd235483eeee445bd
Kade Williams <kwilliams@MTMRec.com>
2026-05-01 10:11:16 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+//server.AcceptSocket(); // wait for client
 
 using var client = server.AcceptTcpClient();
 
