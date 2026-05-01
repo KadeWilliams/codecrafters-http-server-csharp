8126b2f23345efb66ec268c0fbae686cad065bcd
Kade Williams <kwilliams@MTMRec.com>
2026-05-01 10:20:05 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,13 +7,12 @@ Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-//server.AcceptSocket(); // wait for client
 
 using var client = server.AcceptTcpClient();
 
 var stream = client.GetStream();
 
-string response = "HTTP/1.1 200 OK\r\n\r\n";
+string response = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n";
 var encodedResponse = Encoding.UTF8.GetBytes(response);
 
 await stream.WriteAsync(encodedResponse, 0, encodedResponse.Length);
