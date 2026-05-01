22d5db17e08c8395bc0574de2ead01ce63f16f5a
Kade Williams <kwilliams@MTMRec.com>
2026-05-01 10:02:10 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -12,5 +13,8 @@ using var client = server.AcceptTcpClient();
 
 var stream = client.GetStream();
 
-stream.Write(new byte[256]);
+string response = "HTTP/1.1 200 OK\r\n\r\n";
+var encodedResponse = Encoding.ASCII.GetBytes(response);
+
+stream.Write(encodedResponse);
 stream.Flush();
