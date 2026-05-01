e4015150d2b0d4af84edc81334fcf1d388b8d88e
Kade Williams <kwilliams@MTMRec.com>
2026-05-01 11:06:37 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -11,11 +11,16 @@ server.Start();
 using var client = server.AcceptTcpClient();
 var stream = client.GetStream();
 
-var ba = new Memory<byte>();
-var d = await stream.ReadAsync(ba);
-Console.WriteLine(d);
+var ba = new byte[256];
+await stream.ReadAsync(ba);
+Console.WriteLine(Encoding.ASCII.GetString(ba));
 
-string response = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n";
-var encodedResponse = Encoding.UTF8.GetBytes(response);
+//var mba = new Span<byte>();
+//await stream.ReadAsync(mba);
+//Encoding.ASCII.GetString(mba);
 
-await stream.WriteAsync(encodedResponse, 0, encodedResponse.Length);
+
+//string response = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n";
+//var encodedResponse = Encoding.ASCII.GetBytes(response);
+
+//await stream.WriteAsync(encodedResponse, 0, encodedResponse.Length);
