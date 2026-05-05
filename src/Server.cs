8907d5263c9ee5e26eab04e500d171bf6608183d
Kade Williams <kwilliams@MTMRec.com>
2026-05-05 15:08:29 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,12 +7,6 @@ using System.Text.Json;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-//Console.WriteLine($"All args: {JsonSerializer.Serialize(args)}");
-var root = "";
-if (args[0] == "--directory")
-{
-    root = args[1];
-}
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
@@ -23,6 +17,13 @@ while (true)
     Task.Run(async () =>
     {
 
+        //Console.WriteLine($"All args: {JsonSerializer.Serialize(args)}");
+        var root = "";
+        if (args[0] == "--directory")
+        {
+            root = args[1];
+            Console.WriteLine(root);
+        }
         var stream = client.GetStream();
         var buffer = new byte[1024];
         int bytesRead = await stream.ReadAsync(buffer);
