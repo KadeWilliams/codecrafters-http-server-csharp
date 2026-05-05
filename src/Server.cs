544eca1b6949836ab3a3c1be39f81f3f2ac1dd00
Kade Williams <kwilliams@MTMRec.com>
2026-05-05 13:10:22 -0500

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -12,57 +12,60 @@ server.Start();
 
 while (true)
 {
-    using var client = server.AcceptTcpClient();
-    var stream = client.GetStream();
-    var buffer = new byte[1024];
-    int bytesRead = await stream.ReadAsync(buffer);
-
-    var request = Encoding.ASCII.GetString(buffer, 0, bytesRead).TrimEnd('\0');
-    var requestElements = request.Split("\n");
-
-    var outDict = new Dictionary<string, string>();
-    foreach (var (v, i) in requestElements.Select((v, i) => (v, i)))
+    await Task.Run(async () =>
     {
-        if (i == 0)
+        using var client = server.AcceptTcpClient();
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
+        int bytesRead = await stream.ReadAsync(buffer);
+
+        var request = Encoding.ASCII.GetString(buffer, 0, bytesRead).TrimEnd('\0');
+        var requestElements = request.Split("\n");
+
+        var outDict = new Dictionary<string, string>();
+        foreach (var (v, i) in requestElements.Select((v, i) => (v, i)))
         {
-            outDict.Add("endpoint", v.Split(" ")[1]);
-            continue;
+            if (i == 0)
+            {
+                outDict.Add("endpoint", v.Split(" ")[1]);
+                continue;
+            }
+
+            switch (v)
+            {
+                case var s when s.StartsWith("User-Agent"):
+                    var agent = s.Split(" ")[1];
+                    outDict.Add("User-Agent", agent);
+                    break;
+            }
+        }
+        string output;
+
+        var endpoint = outDict["endpoint"];
+        if (endpoint.Contains("echo"))
+        {
+            output = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {endpoint.Split("/")[2].Length}\r\n\r\n{endpoint.Split("/")[2]}";
+        }
+        else if (endpoint == "/")
+        {
+            output = "HTTP/1.1 200 OK\r\n\r\n";
+        }
+        else if (endpoint == "/user-agent")
+        {
+            var userAgent = outDict["User-Agent"];
+            output = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgent.Trim().Length}\r\n\r\n{userAgent}";
+        }
+        else
+        {
+            output = "HTTP/1.1 404 Not Found\r\n\r\n";
         }
 
-        switch (v)
-        {
-            case var s when s.StartsWith("User-Agent"):
-                var agent = s.Split(" ")[1];
-                outDict.Add("User-Agent", agent);
-                break;
-        }
-    }
-    string output;
+        //Console.WriteLine(JsonSerializer.Serialize(outDict));
 
-    var endpoint = outDict["endpoint"];
-    if (endpoint.Contains("echo"))
-    {
-        output = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {endpoint.Split("/")[2].Length}\r\n\r\n{endpoint.Split("/")[2]}";
-    }
-    else if (endpoint == "/")
-    {
-        output = "HTTP/1.1 200 OK\r\n\r\n";
-    }
-    else if (endpoint == "/user-agent")
-    {
-        var userAgent = outDict["User-Agent"];
-        output = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgent.Trim().Length}\r\n\r\n{userAgent}";
-    }
-    else
-    {
-        output = "HTTP/1.1 404 Not Found\r\n\r\n";
-    }
-
-    //Console.WriteLine(JsonSerializer.Serialize(outDict));
-
-    var encodedResponse = Encoding.ASCII.GetBytes(output);
-    await stream.WriteAsync(encodedResponse, 0, encodedResponse.Length);
-    client.Close();
+        var encodedResponse = Encoding.ASCII.GetBytes(output);
+        await stream.WriteAsync(encodedResponse, 0, encodedResponse.Length);
+        client.Close();
+    });
 }
 
 
